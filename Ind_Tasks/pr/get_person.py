c338265d20f42b3d58c56e264786e4ac51239248
pynikacmd <ver.r762@gmail.com>
2022-12-26 01:42:05 +0300

    add

---
+++ Ind_Tasks/pr/get_person.py
@@ -0,0 +1,18 @@
+import datetime
+
+
+def get_person():
+    """"
+    Запросить данные о человеке.
+    """
+    name = input("Фамилия Имя: ")
+    number = int(input("Номер телефона: "))
+    bday = list(map(int, input("Дата рождения: ").split('.')))
+    d_bday = datetime.date(bday[2], bday[1], bday[0])
+
+    # Создать словарь.
+    return {
+        'name': name,
+        'number': number,
+        'birthday': d_bday,
+    }
---
+++ Ind_Tasks/pr/main.py
@@ -0,0 +1,55 @@
+import sys
+from pr.get_person import get_person
+from pr.display_people import display_people
+from pr.find_nomer import find_nomer
+
+
+def main():
+    """
+    Главная функция программы.
+    """
+    # Список людей.
+    people = []
+
+    # Организовать бесконечный цикл запроса команд.
+    while True:
+        # Запросить команду из терминала.
+        command = input("Введите команду >>> ").lower()
+
+        # Выполнить действие в соответствие с командой.
+        if command == 'exit':
+            break
+
+        elif command == 'add':
+            # Запросить данные о человеке.
+            person = get_person()
+
+            # Добавить в словарь список.
+            people.append(person)
+            # Отсортировать список в случае необходимости.
+            if len(people) > 1:
+                people.sort(key=lambda item: item.get('d_bday', ''))
+
+        elif command == 'list':
+            # Отобразить всех людей.
+            display_people(people)
+
+        elif command == 'find':
+            n = int(input('Введите номер телефона: '))
+
+            # Выбрать людей с заданной фамилией.
+            finded = find_nomer(people, n)
+            # Отобразить выбранных работников.
+            display_people(finded)
+
+        elif command == 'help':
+            # Вывести справку о работе с программой.
+            print("Список команд:\n"
+                  "add - добавить человека;\n"
+                  "list - вывести список людей;\n"
+                  "find - найти человека по фамилии;\n"
+                  "help - отобразить справку;\n"
+                  "exit - завершить работу с программой.\n")
+
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
